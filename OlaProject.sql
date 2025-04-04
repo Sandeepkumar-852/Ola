a3fda8a441a50d71e7ebfa9da829fb08f0d8b4d1
Sandeepkumar-852 <129817688+Sandeepkumar-852@users.noreply.github.com>
2025-04-04 08:19:00 +0530

    Add files via upload

---
+++ OlaProject.sql
@@ -0,0 +1,27 @@
+#1. Retrieve all successful bookings:
+select * from Successful_bookings;
+
+#2. Find the average ride distance for each vehicle type:
+select * from ride_distance_for_each_vehicle;
+
+#3. Get the total number of cancelled rides by customers:
+select * from bookings_cancelled_by_customers;
+
+#4. List the top 5 customers who booked the highest number of rides:
+select * from top_5_customers;
+
+#5. Get the number of rides cancelled by drivers due to personal and car-related issues:
+select * from Cancelled_rides_by_driver_due_to_PersonalorCar_issues;
+
+#6. Find the maximum and minimum driver ratings for Prime Sedan bookings:
+select * from max_min_rating;
+
+#7. Find the average customer rating per vehicle type:
+select * from Avg_vehicle_rating;
+
+#8. Calculate the total booking value of rides completed successfully:
+select * from total_successful_ride;
+
+#9. List all incomplete rides along with the reason:
+select * from Incomplete_rides_reason;
+
